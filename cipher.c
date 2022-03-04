3338a26221547be2860cd0e184f66d39940cc107
Chris <kispi@naver.com>
2022-03-04 15:27:24 +0900

    fix: do infinite writing until there is not enough space

--- cipher.c
+++ cipher.c
@@ -4,14 +4,15 @@
 
 const int BYTE_MAX = 255;
 
-void write_bytes(int value, int num_bytes) {
-    FILE* fp = fopen("NONAME", "ab");
-    for (int i = 0; i < num_bytes; i++) {
+void write_bytes(int value) {
+    FILE* fp = fopen("NONAME", "wb"); // TODO: filename's better be UUIDV4 or sth like that in order to prevent deleting existing important user file.
+    int written = -1;
+    while (written != 0) {
         int num_to_write = value;
         if (value == -1) {
             num_to_write = rand() % (BYTE_MAX + 1);
         }
-        fwrite(&num_to_write, 1, 1, fp);
+        written = fwrite(&num_to_write, 1, 1, fp);
     }
     fclose(fp);
 }
@@ -20,15 +21,15 @@ void doThreePass() {
     clock_t t;
     t = clock();
 
-    write_bytes(0, 100);
+    write_bytes(0);
     double elapsed_time = ((double)(clock() - t) / CLOCKS_PER_SEC);
     printf("Elapsed Time (Zero Fill): %f\n", elapsed_time);
 
-    write_bytes(BYTE_MAX, 100);
+    write_bytes(BYTE_MAX);
     elapsed_time = ((double)(clock() - t) / CLOCKS_PER_SEC);
     printf("Elapsed Time (Complementary Fill): %f\n", elapsed_time);
 
-    write_bytes(-1, 100);
+    write_bytes(-1);
     elapsed_time = ((double)(clock() - t) / CLOCKS_PER_SEC);
     printf("Elapsed Time (Random Byte Fill): %f\n", elapsed_time);
 }
